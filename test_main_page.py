1c023720b26f0b23ff205c3991650bfc754b9c98
vasvap <vasvap@gmail.com>
2024-05-02 18:04:06 +0300

    first page object added

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,20 @@
-from selenium.webdriver.common.by import By
+import pytest
+import sys
+import os
+from pages.main_page import MainPage
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-def test_guest_can_go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+def test_guest_can_go_to_login_page(browser, request):
+    lang = request.config.getoption('language')
+    if lang == "en":
+        link = 'http://selenium1py.pythonanywhere.com/en-gb/'
+    elif lang == 'ru':
+        link = 'http://selenium1py.pythonanywhere.com/ru/'
+    else:
+        raise pytest.UsageError('--language should be en or ru')
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
